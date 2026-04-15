8d4a8e01dd79901881e9ccc96020a6dfcb013bc1
Mugil-Gokul <gokult@mugil.io>
2026-04-15 15:23:24 +0530

    Env Done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-500 text-2xl'>Welcome to Ampra</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
